60b17eafac4cddd594d39b5f6dd540d972649bf6
Bulut Yerli <bulutyerli@gmail.com>
2023-08-07 15:08:45 +0300

    created dogs and cats pages with pagination

--- src/app/breeds/cats/[id]/page.jsx
+++ src/app/breeds/cats/[id]/page.jsx
@@ -3,10 +3,11 @@ import styles from '@/app/breeds/breeds.module.scss';
 import notFound from 'public/images/catNotFound.png';
 import Link from 'next/link';
 import Pagination from '@/components/pagination/pagination';
-
-const apiKey = process.env.CAT_API_KEY;
+import LetterFilter from '@/components/letterFilter/letterFilter';
 
 export async function getCats() {
+  const apiKey = process.env.CAT_API_KEY;
+
   try {
     const response = await fetch('https://api.thecatapi.com/v1/breeds', {
       headers: { 'x-api-key': apiKey },
@@ -16,31 +17,29 @@ export async function getCats() {
       throw new Error('something went wrong');
     }
 
-    const totalData = catData.length;
-    const dataPerPage = 15;
-    pageNum: Math.ceil(totalData / dataPerPage);
     return catData;
   } catch (error) {
     console.log('something went wrong', error);
   }
 }
 
-export default async function BreedsPage({ params }) {
+async function BreedsPage({ params }) {
   const cats = await getCats();
-  console.log('data fetch is done');
+  console.log('fetched data here');
   const totalData = cats.length;
   const dataPerPage = 15;
   const totalPages = Math.ceil(totalData / dataPerPage);
-  let currentPage = params.pageNum;
+  let currentPage = params.id;
   const startIndex = (currentPage - 1) * dataPerPage;
   const endIndex = startIndex + dataPerPage;
-
   return (
     <section className={styles.container}>
       <h1>Cat Breeds </h1>
+      <LetterFilter type="cats" />
       <ul className={styles.cardContainer}>
         {cats.slice(startIndex, endIndex).map((cat) => {
           let imageUrl = cat.image?.url || notFound;
+          console.log(params.id);
 
           return (
             <li key={cat.id}>
@@ -54,8 +53,21 @@ export default async function BreedsPage({ params }) {
       <Pagination
         totalPages={totalPages}
         currentPage={currentPage}
-        type={'cats'}
+        type="cats"
       />
     </section>
   );
+}
+
+export default BreedsPage;
+
+export async function generateStaticParams() {
+  const cards = await getCats();
+  const totalData = cards.length;
+  const dataPerPage = 15;
+  const totalPages = Math.ceil(totalData / dataPerPage);
+
+  return Array.from({ length: totalPages }, (_, index) => ({
+    id: (index + 1).toString(),
+  }));
 }

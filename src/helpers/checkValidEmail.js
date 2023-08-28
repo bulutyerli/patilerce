0b56a4db97ca36b056bc761cf1ba8f4f20cb0d3f
Bulut Yerli <bulutyerli@gmail.com>
2023-08-28 16:02:58 +0300

    created sign up api, connection to database and design of sign up page with error handling

---
+++ src/helpers/checkValidEmail.js
@@ -0,0 +1,7 @@
+export default function checkValidEmail(email) {
+  const validEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/.test(
+    email
+  );
+
+  if (validEmail) return true;
+}

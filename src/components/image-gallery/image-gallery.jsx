8dde98195b2ba3bda10e4c0859ba97483be95c8d
bulut yerli <bulut@buluts-Mac-mini.local>
2024-06-26 21:41:16 +0300

    fixed image src issue

--- src/components/image-gallery/image-gallery.jsx
+++ src/components/image-gallery/image-gallery.jsx
@@ -1,22 +1,20 @@
 'use client';
 
 import styles from './image-gallery.module.scss';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import imageKitLoader from '@/lib/imageKitLoader/imageLoader';
 
 export default function ImageGallery({ images, breed }) {
-  const [mainImage, setMainImage] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     if (images.length > 0) {
-      setMainImage(images[0]);
+      setActiveIndex(0);
     }
-  }, []);
+  }, [images]);
 
   const mainImageHandler = (index) => {
-    setMainImage(images[index]);
     setActiveIndex(index);
   };
 
@@ -29,32 +27,27 @@ export default function ImageGallery({ images, breed }) {
       <div className={styles.mainImage}>
         <Image
           loader={imageKitLoader}
-          src={mainImage}
+          src={images[activeIndex]}
           alt={breed}
           width={300}
           height={225}
-        ></Image>
+        />
       </div>
       <div className={styles.imageThumbnails}>
-        {images.map((image, index) => {
-          const isActive = activeIndex === index;
-          return (
-            <Image
-              loader={imageKitLoader}
-              onClick={() => {
-                mainImageHandler(index);
-              }}
-              className={`${styles.imageThumbnail} ${
-                isActive ? styles.activeImage : ''
-              }`}
-              key={index}
-              height={50}
-              width={50}
-              alt={breed}
-              src={image}
-            ></Image>
-          );
-        })}
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            loader={imageKitLoader}
+            onClick={() => mainImageHandler(index)}
+            className={`${styles.imageThumbnail} ${
+              activeIndex === index ? styles.activeImage : ''
+            }`}
+            height={50}
+            width={50}
+            alt={breed}
+            src={image}
+          />
+        ))}
       </div>
     </div>
   );
